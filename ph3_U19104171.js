807f8e42eb19d7ef765cc7d4e9825bfe029b820e
Maitha2001 <113858668+Maitha2001@users.noreply.github.com>
2022-11-27 19:32:25 +0400

    Add files via upload

---
+++ ph3_U19104171.js
@@ -0,0 +1,14 @@
+<script>
+
+var modal = document.getElementById('signin');
+
+window.onclick = function(event)
+{
+    if (event.target == form)
+    {
+        modal.style.display = "none";
+    }
+}
+
+
+</script>
